361f96743fc00355cbc8f051face606fc3a87b31
Judah <i460958@FontysICT.nl>
2025-04-09 04:31:04 +0200

    Changed many things

--- analytics-service/cmd/app/main.go
+++ analytics-service/cmd/app/main.go
@@ -27,23 +27,15 @@ func run() error {
     appName := os.Getenv("APP_NAME")
     rabbitMQConnectionString := os.Getenv("RABBITMQ_CONNECTION_STRING")
 
-    telemetryEnabled := true
-    disable_telemetry := os.Getenv("DISABLE_TELEMETRY")
-    if disable_telemetry == "true" {
-        telemetryEnabled = false
+    telemetryShutdown, err := telemetry.InitTelemetry(ctx, appName)
+    if err != nil {
+        return err
     }
-        
-    if telemetryEnabled {
-        telemetryShutdown, err := telemetry.InitTelemetry(ctx, appName)
-        if err != nil {
-            return err
+    defer func() {
+        if shutdownErr := telemetryShutdown(context.Background()); shutdownErr != nil {
+            slog.Error("Telemetry shutdown failed", slog.Any("error", shutdownErr))
         }
-        defer func() {
-            if shutdownErr := telemetryShutdown(context.Background()); shutdownErr != nil {
-                slog.Error("Telemetry shutdown failed", slog.Any("error", shutdownErr))
-            }
-        }()
-    }
+    }()
 
     db_err := repository.InitTables()
     util.FailOnError(db_err, "Failed to initialize database tables")
